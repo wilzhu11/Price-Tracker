6aad1d9f93e475a9a700f39d9b63b69bffabf79a
wilzhu11 <t2dc75xdjm@privaterelay.appleid.com>
2025-12-04 16:00:12 -0600

    tools

---
+++ main.py
@@ -0,0 +1,13 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from pathlib import Path
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+
+
+
+
